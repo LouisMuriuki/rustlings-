8232aca53f5e4d0eae4050ccd02c1785ce9f6040
luihugo <louish@cashlet.co.ke>
2024-06-26 23:09:19 +0300

    test quizes done

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -6,12 +6,17 @@
 // Execute `rustlings hint tests2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
+fn add(value:i32)->i32{
+    value*2
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        assert_eq!(4,add(2));
     }
 }
